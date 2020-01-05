8d720b5db8c249f6e5d13f082477731036805a23
smf8 <mr.smf8@gmail.com>
2020-01-05 14:01:01 +0330

    add password validation functions

--- model/user.go
+++ model/user.go
@@ -1,10 +1,34 @@
 package model
 
-import "github.com/jinzhu/gorm"
+import (
+	"errors"
+	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
+)
 
 type User struct {
 	gorm.Model
 	Username string `gorm:"unique_index;not null"`
 	Password string `gorm:"not null"`
 	Urls     []Url  `gorm:"foreignkey:user_id"`
+}
+
+func NewUser(username, password string) (*User, error) {
+	if len(password) == 0 {
+		return nil, errors.New("Password cannot be empty")
+	}
+	pass, _ := HashPassword(password)
+	return &User{Username: username, Password: pass}, nil
+}
+
+func HashPassword(pass string) (string, error) {
+	if len(pass) == 0 {
+		return "", errors.New("Password cannot be empty")
+	}
+	hash, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.MinCost)
+	return string(hash), err
+}
+
+func (user *User) ValidatePassword(pass string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(pass)) == nil
 }
