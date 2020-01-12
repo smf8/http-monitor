b113d64aaf9c9ea1762f509d99e1854fa75b84f6
smf8 <mr.smf8@gmail.com>
2020-01-13 01:32:52 +0330

    add get request on URL

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/smf8/http-monitor
 go 1.12
 
 require (
+	github.com/gammazero/workerpool v0.0.0-20200108033143-79b2336fad7a
 	github.com/jinzhu/gorm v1.9.11
 	github.com/kr/pretty v0.2.0 // indirect
+	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0
 	github.com/stretchr/testify v1.4.0
 	golang.org/x/crypto v0.0.0-20190325154230-a5d413f7728c
--- model/url.go
+++ model/url.go
@@ -3,7 +3,9 @@ package model
 import (
 	"errors"
 	"github.com/jinzhu/gorm"
+	"net/http"
 	"regexp"
+	"strings"
 )
 
 type URL struct {
@@ -22,15 +24,18 @@ type Request struct {
 }
 
 //NewURL creates a URL instance if it's address is a valid URL address
-func NewURL(userID uint, address string, threshold, failedTimes int) (*URL, error) {
+func NewURL(userID uint, address string, threshold int) (*URL, error) {
 	url := new(URL)
 	url.UserId = userID
 	url.Threshold = threshold
-	url.FailedTimes = failedTimes
+	url.FailedTimes = 0
 
 	reg, err := regexp.Compile(`^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$`)
 	if err != nil {
 		return nil, err
+	}
+	if !strings.HasPrefix("http://", address) {
+		address = "http://" + address
 	}
 	if reg.MatchString(address) {
 		//valid URL address
@@ -45,4 +50,17 @@ func NewURL(userID uint, address string, threshold, failedTimes int) (*URL, erro
 // Use this function to check alarm and trigger an alarm with other functions
 func (url *URL) ShouldTriggerAlarm() bool {
 	return url.FailedTimes >= url.Threshold
+}
+
+// SendRequest sends a HTTP GET request to the url
+// returns a *Request with result status code
+func (url *URL) SendRequest() (*Request, error) {
+	resp, err := http.Get(url.Address)
+	req := new(Request)
+	req.UrlId = url.ID
+	if err != nil {
+		return req, err
+	}
+	req.Result = resp.StatusCode
+	return req, nil
 }
